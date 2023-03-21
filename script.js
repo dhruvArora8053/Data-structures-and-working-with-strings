df5c575aa57be4d46d19b8b659355a294e8281c9
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-21 18:26:25 +0530

    challenge done

--- script.js
+++ script.js
@@ -84,4 +84,54 @@ console.log(typeof new String('jonas')); //otput: object
 
 //and infact all string methods return primitives even if called on string object:
 console.log(typeof new String('jonas').slice(1));
-//and so the result of all of this is then back to being a string 
+//and so the result of all of this is then back to being a string
+
+// Write a program that receives a list of variable names written in underscore_case
+// and convert them to camelCase.
+// The input will come from a textarea inserted into the DOM (see code below to
+// insert the elements), and conversion will happen when the button is pressed.
+// Test data (pasted to textarea, including spaces):
+// underscore_case
+// first_name
+// Some_Variable
+//  calculate_AGE
+// delayed_departure
+// Should produce this output (5 separate console.log outputs):
+// underscoreCase ✅
+// firstName ✅✅
+// someVariable ✅✅✅
+// calculateAge ✅✅✅✅
+// delayedDeparture ✅✅✅✅✅
+// Hints:
+// § Remember which character defines a new line in the textarea �
+// § The solution only needs to work for a variable made out of 2 words, like a_b
+// § Start without worrying about the ✅. Tackle that only after you have the variable
+// name conversion working �
+// § This challenge is difficult on purpose, so start watching the solution in case
+// you're stuck. Then pause and continue!
+// Afterwards, test with your own test data!
+// GOOD LUCK �
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const arr = text.split('\n');
+
+  for (const [i, t] of arr.entries()) {
+    let [a, b] = t.split('_');
+    b = b.replace(b[0], b[0].toUpperCase());
+    const join = a + b;
+    // console.log(join);
+    const trim = join.trim();
+    // console.log(trim);
+
+    const twenty = trim.padEnd(20);
+    // console.log(twenty);
+
+    const last = `${twenty}${'✅'.repeat(i + 1)}`;
+
+    console.log(last);
+  }
+});
